e8a48fac083d1382bffc724952f28365b338b373
bhargabmedhi <126895556+bhargabmedhi@users.noreply.github.com>
2024-06-08 15:23:12 +0530

    Add files via upload

---
+++ SQLQuery2(HZL_table).sql
@@ -0,0 +1,8 @@
+SELECT * FROM HZL_Table
+UPDATE HZL_Table
+set value=3456
+where BU='hzl',
+set value=32456
+where date between '2024-01-01' and '2024-04-01',
+Set value=345
+Where Date='2024-06-01'
